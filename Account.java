181aa7d16cb09f861e21a8f743e57ab0bb803aa7
awayse15 <164579525+awayse15@users.noreply.github.com>
2024-03-31 11:44:42 -0400

    Uploaded Account class

---
+++ Account.java
@@ -0,0 +1,114 @@
+import java.util.ArrayList;
+
+public class Account implements AccountInterface {
+    private String name;
+    private String password;
+    private ArrayList<Account> friends;
+    private ArrayList<Account> blocked;
+    private boolean friendsOnly;
+
+    public Account(String data) {
+        String[] parts = data.split(":");
+        String[] info = parts[0].split(",");
+        this.name = info[0];
+        this.password = info[1];
+        this.friendsOnly = Boolean.parseBoolean(info[2]);
+
+        String[] friendNames = parts[1].split(",");
+        this.friends = new ArrayList<>();
+        for (String friendName : friendNames) {
+            if (!friendName.isEmpty()) {
+                this.friends.add(new Account(friendName));
+            }
+        }
+
+        String[] blockedNames = parts[2].split(",");
+        this.blocked = new ArrayList<>();
+        for (String blockedName : blockedNames) {
+            if (!blockedName.isEmpty()) {
+                this.blocked.add(new Account(blockedName));
+            }
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFriends(ArrayList<Account> friends) {
+        this.friends = friends;
+    }
+
+    public void setFriendsOnly(boolean friendsOnly) {
+        this.friendsOnly = friendsOnly;
+    }
+
+    public boolean getFriendsOnly() {
+        return friendsOnly;
+    }
+
+    public ArrayList<Account> getFriends() {
+        return friends;
+    }
+
+    public boolean addFriend(Account friend) {
+        if (!blocked.contains(friend)) {
+            return friends.add(friend);
+        }
+        return false;
+    }
+
+    public boolean removeFriend(Account friend) {
+        return friends.remove(friend);
+    }
+
+    public ArrayList<Account> getBlocked() {
+        return blocked;
+    }
+
+    public void setBlocked(ArrayList<Account> blocked) {
+        this.blocked = blocked;
+    }
+
+    public boolean addBlocked(Account blocked) {
+        if (!friends.contains(blocked)) {
+            return this.blocked.add(blocked);
+        }
+        return false;
+    }
+
+    public boolean removeBlocked(Account blocked) {
+        return this.blocked.remove(blocked);
+    }
+
+    public boolean equals(Account account) {
+        return this.name.equals(account.getName());
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(name).append(",").append(password).append(":");
+
+        for (Account friend : friends) {
+            stringBuilder.append(friend.getName()).append(",");
+        }
+        stringBuilder.append(":");
+
+        for (Account blocked : blocked) {
+            stringBuilder.append(blocked.getName()).append(",");
+        }
+        return stringBuilder.toString();
+    }
+}
